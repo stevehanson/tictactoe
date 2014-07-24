427697bbb958c8915d269a77ec4232a0a953343a
Steve Hanson <shanson@Steves-MacBook-Pro.local>
2014-07-24 02:23:50 -0500

    Clean up code

--- tictactoe.rb
+++ tictactoe.rb
@@ -1,59 +1,53 @@
-# original
+# simple tic tac toe game to demonstrate SRP principles
 
 class TicTacToe
-
-  attr_reader :board
 
   def initialize
     @board = Array.new(3) {
       Array.new(3) { ' ' }
     }
-    @current_player = 1
+    @current_player = 'x'
   end
 
-  def play_turn(square)
-    @board[square[0]][square[1]] = (@current_player == 1) ? 'x' : 'o'
-    @current_player = (@current_player==1) ? 0 : 1
+  def play_turn(row, col)
+    @board[row][col] = @current_player
     print_board
     check_winner
+    toggle_player
+  end
+
+  def toggle_player
+    @current_player = (@current_player=='x') ? 'o' : 'x'
   end
 
   def print_board
     @board.each do |row|
-      row.each do |cell|
-        print cell + ' '
-      end
-      puts
+      puts row.join(' ')
     end
   end
 
   def check_winner
-    result = check_rows
-    if(result) then
-      puts result + ' wins!!'
-    end
-    result = check_columns
-    if(result) then
-      puts result + ' wins!!'
+    if(rows_winner? or cols_winner?) then
+      puts @winner + ' wins!!'
     end
   end
 
-  def check_rows
-    @board.each do |(x,y,z)|
-      if(x!=' ' && x == y && x == z) then
-        return x
-      end
-    end
-    return false
+  def rows_winner?
+    all_equal_in_group?(@board)
   end
 
-  def check_columns
-    (0..2).each do |n|
-      if(@board[0][n] != ' ' && @board[0][n] == @board[1][n] && @board[0][n] == @board[2][n]) then
-        return @board[n][0]
+  def cols_winner?
+    all_equal_in_group?(@board.transpose)
+  end
+
+  def all_equal_in_group?(groups)
+    groups.each do |group|
+      if(group[0] != ' ' and group.uniq.length == 1) then
+        @winner = group[0]
+        return true
       end
     end
-    return false
+    false
   end
 
 end
